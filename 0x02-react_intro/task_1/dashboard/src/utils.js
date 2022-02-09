ca928e63877bd6276c1ef364c3e31f886832600f
Aomine Daiki <aominedaikihacking@gmail.com>
2022-02-09 17:44:32 +0100

    task 1

---
+++ 0x02-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,10 @@
+function getFullYear() {
+    return (new Date().getFullYear());
+}
+
+function getFooterCopy(isIndex) {
+    if (isIndex) return "Holberton School";
+    else return "Holberton School main dashboard"
+}
+
+export {getFullYear, getFooterCopy};
