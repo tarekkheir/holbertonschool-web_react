5d93de125776d7fc483cf2ba9c81f1a3da5940d9
Aomine Daiki <aominedaikihacking@gmail.com>
2022-02-11 14:19:14 +0100

    Updates

---
+++ precache-manifest.846cd856d28081bcf9991c05c0cf9649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf7a6520b77fd98daeba0b4c531ae19",
+    "url": "/react-gh-page/index.html"
+  },
+  {
+    "revision": "5351de51e6d6615772de",
+    "url": "/react-gh-page/static/css/main.3c275e57.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/react-gh-page/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "5351de51e6d6615772de",
+    "url": "/react-gh-page/static/js/main.3fda9e62.chunk.js"
+  },
+  {
+    "revision": "91fb36e7fd4fab64fc8e",
+    "url": "/react-gh-page/static/js/runtime~main.b97fe780.js"
+  },
+  {
+    "revision": "23d20eb3253de8404d66401eb6db58d5",
+    "url": "/react-gh-page/static/media/holberton_logo.23d20eb3.jpg"
+  }
+]);
