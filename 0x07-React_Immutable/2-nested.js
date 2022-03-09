e24aac25f5a7895d2099b9c0bbc366b6bcda9d18
Tarek <kheirtarek@hotmail.fr>
2022-03-09 23:12:56 +0100

    all tasks

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    const mappedObj = fromJS(object);
+  const mappedObj = fromJS(object);
 
-    return mappedObj.getIn(array, undefined);
+  return mappedObj.getIn(array, undefined);
 }
