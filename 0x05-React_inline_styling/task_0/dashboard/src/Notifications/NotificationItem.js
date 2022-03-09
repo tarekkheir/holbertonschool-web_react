8ddbb9a4431349d9e0117dc48e2758e213310d93
Tarek <kheirtarek@hotmail.fr>
2022-03-09 17:43:13 +0100

    task 0

---
+++ 0x05-React_inline_styling/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import propTypes from "prop-types";
+import { markAsRead } from "./Notifications";
+
+class NotificationItem extends React.PureComponent {
+  render() {
+    if (this.props.html) {
+      return <li
+        data-notification-type={this.props.type}
+        dangerouslySetInnerHTML={this.props.html}
+        onClick={() => this.props.markAsRead(this.props.id)} />;
+    } else {
+      return <li
+        data-notification-type={this.props.type}
+        onClick={() => this.props.markAsRead(this.props.id)}>
+        {this.props.value}
+      </li>;
+    }
+  }
+}
+NotificationItem.defaultProps = {
+  type: "default"
+}
+
+NotificationItem.propTypes = {
+  html: propTypes.shape({
+    __html: propTypes.string
+  }),
+  type: propTypes.string,
+  value: propTypes.string,
+  id: propTypes.oneOfType([propTypes.string, propTypes.number]),
+  markAsRead: propTypes.func
+}
+
+export default NotificationItem;
