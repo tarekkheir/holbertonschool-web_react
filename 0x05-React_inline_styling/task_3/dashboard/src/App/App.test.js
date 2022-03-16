c80c2c144b68e754b1a5443425bda3fc894c3ace
Tarek <kheirtarek@hotmail.fr>
2022-03-16 13:47:36 +0100

    task 3

---
+++ 0x05-React_inline_styling/task_3/dashboard/src/App/App.test.js
@@ -0,0 +1,69 @@
+import { shallow } from 'enzyme';
+import App from "./App";
+import React from 'react';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+
+beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("App tests", () => {
+    it("render without crash", () => {
+        shallow(<App />);
+    })
+
+    it("Notifications component", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Notifications')).toHaveLength(1);
+    });
+
+    it("Header component", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Header')).toHaveLength(1);
+    });
+
+    it("Login component", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Login')).toHaveLength(1);
+    });
+
+    it("Footer component", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Footer')).toHaveLength(1);
+    });
+
+    it("CourseList not display", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('CourseList')).toHaveLength(0);
+        expect(wrapper.find('Login')).toHaveLength(1);
+    });
+
+    it("App with isLoggedIn true", () => {
+        const wrapper = shallow(<App isLoggedIn={true} />);
+        expect(wrapper.find('Login')).toHaveLength(0);
+        expect(wrapper.find('CourseList')).toHaveLength(1);
+    });
+
+    it("press h and ctrl down", () => {
+        const events = {};
+        const logOut = jest.fn();
+
+        window.alert = jest.fn();
+        window.addEventListener = jest.fn((event, cb) => {
+            events[event] = cb;
+        });
+
+        shallow(<App logOut={logOut} />);
+        events.keydown({ key: 'h', ctrlkey: true });
+
+        expect(logOut).toBeCalledTimes(1);
+        expect(window.alert).toBeCalledWith('Logging you out');
+
+        window.alert.mockRestore();
+    });
+});
---
+++ 0x05-React_inline_styling/task_3/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import Notifications from './Notifications';
+import { shallow } from 'enzyme';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Notifications tests", () => {
+  it("render without fail", () => {
+    const wrapper = shallow(<Notifications />);
+  })
+
+  // it("Notifications not display", () => {
+  //   const wrapper = shallow(<Notifications />);
+  //   expect(wrapper.find('.menuItem')).toHaveLength(1);
+  //   expect(wrapper.find('.Notifications')).toHaveLength(0);
+  // });
+
+  // it("Notifications display", () => {
+  //   const wrapper = shallow(<Notifications displayDrawer />);
+  //   expect(wrapper.find('.menuItem')).toHaveLength(1);
+  //   expect(wrapper.find('.Notifications')).toHaveLength(1);
+  // });
+
+  // it("empty listNotifications property", () => {
+  //   shallow(<Notifications listNotifications={[]} />);
+  // });
+
+  // it("listNotifications is no empty", () => {
+  //   const listNotifications = [
+  //     { id: 1, value: 'New course available', type: 'default' },
+  //     { id: 2, value: 'New resume available', type: 'urgent' },
+  //     {
+  //       id: 3,
+  //       html: {
+  //         __html: '<strong>Urgent requirement</strong> - complete by EOD',
+  //       },
+  //       type: 'urgent',
+  //     },
+  //   ];
+  //   const wrapper = shallow(
+  //     <Notifications displayDrawer listNotifications={listNotifications} />,
+  //   );
+  //   expect(wrapper.find('div.Notifications')).toHaveLength(1);
+  //   expect(wrapper.find('div.Notifications').children()).toHaveLength(3);
+  // });
+
+  // it('when updating the props of the component with a longer list, the component does rerender', () => {
+  //   const listNotifications = [
+  //     { id: 1, value: 'New course available', type: 'default' },
+  //     { id: 2, value: 'New resume available', type: 'urgent' },
+  //     {
+  //       id: 3,
+  //       html: {
+  //         __html: '<strong>Urgent requirement</strong> - complete by EOD',
+  //       },
+  //       type: 'urgent',
+  //     },
+  //     { id: 4, value: 'New course available', type: 'default' },
+  //     { id: 5, value: 'New resume available', type: 'urgent' },
+  //     {
+  //       id: 6,
+  //       html: {
+  //         __html: '<strong>Urgent requirement</strong> - complete by EOD',
+  //       },
+  //       type: 'urgent',
+  //     },
+  //   ];
+  //   const wrapper = shallow(
+  //     <Notifications displayDrawer listNotifications={listNotifications} />,
+  //   );
+  //   wrapper.setProps({ listNotifications });
+  //   expect(wrapper.find('div.Notifications').length).toBe(1);
+  //   expect(wrapper.find('div.Notifications').children().length).toBe(3);
+  // });
+});
