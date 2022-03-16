831f391ff921e1f6ab03dc206d52d012072b4e23
Tarek <kheirtarek@hotmail.fr>
2022-03-16 13:14:14 +0100

    fix App

--- 0x05-React_inline_styling/task_1/dashboard/src/App/App.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/App/App.js
@@ -1,19 +1,20 @@
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Login from '../Login/Login';
-import Notifications from '../Notifications/Notifications';
-import PropTypes from 'prop-types';
-import CourseList from '../CourseList/CourseList';
+import React from 'react';
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import CourseList from "../CourseList/CourseList";
+import propTypes from "prop-types";
 import { getLatestNotification } from '../utils/utils';
 import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 import BodySection from '../BodySection/BodySection';
-import '../utils/reset.css';
-import { StyleSheet, css } from 'aphrodite';
+import { StyleSheet, css } from "aphrodite";
+
 
 const listCourses = [
   { id: 1, name: 'ES6', credit: 60 },
   { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
+  { id: 3, name: 'React', credit: 40 }
 ];
 
 const listNotifications = [
@@ -37,11 +38,11 @@ class App extends React.Component {
   }
 
   listener = (event) => {
-    if (event.key === 'h' && event.ctrlKey) {
+    if (event.key === 'h' && event.ctrlkey === true) {
       alert('Logging you out');
       this.props.logOut();
     }
-  };
+  }
 
   render() {
     return (
@@ -50,40 +51,45 @@ class App extends React.Component {
         <div className="App">
           <Header />
           <div className={('App-body', css(styles.AppBody))}>
-            {this.props.isLoggedIn ? (
-              <BodySectionWithMarginBottom title="Course list">
+            {this.props.isLoggedIn ?
+              <BodySectionWithMarginBottom title="Course List">
                 <CourseList listCourses={listCourses} />
-              </BodySectionWithMarginBottom>
-            ) : (
+              </BodySectionWithMarginBottom> :
               <BodySectionWithMarginBottom title="Log in to continue">
                 <Login />
-              </BodySectionWithMarginBottom>
-            )}
-            <BodySection title="News from the school">
-              <p>Some random text</p>
+              </BodySectionWithMarginBottom>}
+            <BodySection title="News from the School">
+              <p>Hello World !</p>
             </BodySection>
           </div>
           <Footer />
         </div>
-      </React.Fragment>
+      </React.Fragment >
     );
   }
 }
 
-const styles = StyleSheet.create({
-  AppBody: {
-    padding: '50px',
-  },
-});
-
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => { },
+  logOut: () => { }
 };
 
 App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
+  isLoggedIn: propTypes.bool,
+  logOut: propTypes.func
 };
+
+const styles = StyleSheet.create({
+  AppBody: {
+    padding: '50px'
+  },
+  body: {
+    display: 'inline',
+  },
+
+  footer: {
+    padding: '0',
+  },
+});
 
 export default App;
