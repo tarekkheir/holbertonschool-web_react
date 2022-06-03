4c9f24284d587d101858621eb4619bac218d7ade
Aomine Daiki <aominedaikihacking@gmail.com>
2022-06-03 16:11:33 +0200

    updates

--- 0x09-react_redux_reducer_selector/task_2/dashboard/src/CourseList/CourseShape.js
+++
@@ -1,9 +0,0 @@
-import propTypes from "prop-types";
-
-const CourseShape = propTypes.shape({
-    id: propTypes.number.isRequired,
-    name: propTypes.string.isRequired,
-    credit: propTypes.number.isRequired
-});
-
-export default CourseShape;
